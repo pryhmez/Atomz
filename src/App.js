65121ae4eb7053fa0c5dcbbdee768ec8a2622e9e
chibuokem Jerry <profkiti@gmail.com>
2019-12-14 18:42:21 +0100

    mvp app ready

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import  Landing from "./landing/Landing";
+import Register from "./register/Register"
+import Dashboard from './Dashboard/Dashboard'
+import Emergency from './emergencyList/Emergency'
+import Login from "./login/Login"
+import Alerts from './common/Alerts';
+import PrivateRoute from './routing/PrivateRoute';
+
+import AuthState from './context/auth/AuthState';
+import AlertState from './context/alert/AlertState';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AuthState>
+        <AlertState>
+          <Router >
+            <Alerts />
+            <Switch>
+              <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              <Route exact path="/emergency_list" component={Emergency} />
+              <Route exact path="/" component={Landing} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/register" component={Register} />
+            </Switch>
+          </Router >
+          </AlertState>
+        </AuthState>
     </div>
   );
 }
